01159f3ce0ea5ed45c9b98a282808ec4c67272c2
Stefan Uzunov <andromart.su@gmail.com>
2012-08-22 20:04:26 +0300

    Linux 3.0.25
    Conflicts:
    
    	fs/partitions/check.c
    	net/ipv4/route.c

--- arch/arm/mach-msm/scm.c
+++ arch/arm/mach-msm/scm.c
@@ -36,6 +36,16 @@
 #define SCM_ERROR		-1
 #define SCM_INTERRUPTED		1
 
+#if defined(__GNUC__) && \
+  defined(__GNUC_MINOR__) && \
+  defined(__GNUC_PATCHLEVEL__) && \
+  ((__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)) \
+    >= 40502
+#define USE_ARCH_EXTENSION_SEC 1
+#else
+#define USE_ARCH_EXTENSION_SEC 0
+#endif
+
 static DEFINE_MUTEX(scm_lock);
 
 /**
@@ -180,6 +190,9 @@ static u32 smc(u32 cmd_addr)
 			__asmeq("%1", "r0")
 			__asmeq("%2", "r1")
 			__asmeq("%3", "r2")
+#if USE_ARCH_EXTENSION_SEC
+		        ".arch_extension sec\n"
+#endif
 			"smc	#0	@ switch to secure world\n"
 			: "=r" (r0)
 			: "r" (r0), "r" (r1), "r" (r2)
@@ -279,6 +292,9 @@ u32 scm_get_version(void)
 			__asmeq("%1", "r1")
 			__asmeq("%2", "r0")
 			__asmeq("%3", "r1")
+#if USE_ARCH_EXTENSION_SEC
+			".arch_extension sec\n"
+#endif
 			"smc	#0	@ switch to secure world\n"
 			: "=r" (r0), "=r" (r1)
 			: "r" (r0), "r" (r1)
