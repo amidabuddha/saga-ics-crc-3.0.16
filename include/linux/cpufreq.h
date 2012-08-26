c169dfb2a0b0782a186539a08455c34b86d83bc3
Stefan Uzunov <andromart.su@gmail.com>
2012-08-26 03:27:43 +0300

    Remove pegasusq governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -390,9 +390,6 @@ extern struct cpufreq_governor cpufreq_gov_wheatley;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCARY)
 extern struct cpufreq_governor cpufreq_gov_scary;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_scary)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
-extern struct cpufreq_governor cpufreq_gov_pegasusq;
-#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_pegasusq)
 #endif
 
 
