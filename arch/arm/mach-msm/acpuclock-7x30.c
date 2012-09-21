a7e880eb9f3764eae0ca344343a37b0bc3138624
Stefan Uzunov <andromart.su@gmail.com>
2012-09-21 10:38:55 +0300

    Revert to stock frequency table

--- arch/arm/mach-msm/acpuclock-7x30.c
+++ arch/arm/mach-msm/acpuclock-7x30.c
@@ -89,13 +89,10 @@ static struct clkctl_acpu_speed *backup_s;
 
 static struct pll pll2_tbl[] = {
 	{  42, 0, 1, 0 }, /*  806 MHz */
-	{  48, 1, 3, 0 }, /*  921 MHz */
 	{  53, 1, 3, 0 }, /* 1024 MHz */
-	{  58, 1, 3, 0 }, /* 1113 MHz */
-	{  63, 1, 3, 0 }, /* 1209 MHz */
-	{  68, 1, 3, 0 }, /* 1305 MHz */
-	{  73, 1, 3, 0 }, /* 1401 MHz */
-	{  78, 1, 3, 0 }, /* 1516 MHz */
+	{ 125, 0, 1, 1 }, /* 1200 MHz */
+	{  73, 0, 1, 0 }, /* 1401 MHz */
+	{  78, 0, 1, 0 }, /* 1500 MHz */
 };
 
 /* Use negative numbers for sources that can't be enabled/disabled */
@@ -126,18 +123,16 @@ static struct clkctl_acpu_speed acpu_freq_tbl[] = {
 	{ 0, MAX_AXI_KHZ, AXI, 1, 0, 61440000, 1000, VDD_RAW(1000) },
 	{ 1, 245760, PLL_3,    5, 2,  61440000,  1000, VDD_RAW(1000) },
 	{ 1, 368640, PLL_3,    5, 1,  122800000, 1050, VDD_RAW(1050) },
-	{ 1, 460800,  PLL_1, 2, 0,  153600000, 1050, VDD_RAW(1050) },
-	{ 1, 576000,  PLL_1, 2, 0,  153600000, 1050, VDD_RAW(1050) },
-	{ 1, 652800,  PLL_1, 2, 0,  153600000, 1050, VDD_RAW(1050) },
+	/* AXI has MSMC1 implications. See above. */
 	{ 1, 768000, PLL_1,    2, 0,  153600000, 1100, VDD_RAW(1100) },
+	/*
+	 * AXI has MSMC1 implications. See above.
+	 */
 	{ 1, 806400,  PLL_2, 3, 0, UINT_MAX, 1100, VDD_RAW(1100), &pll2_tbl[0]},
-	{ 1, 921600,  PLL_2, 3, 0, UINT_MAX, 1100, VDD_RAW(1100), &pll2_tbl[1]},
-	{ 1, 1024000, PLL_2, 3, 0, UINT_MAX, 1200, VDD_RAW(1200), &pll2_tbl[2]},
-	{ 1, 1113000, PLL_2, 3, 0, UINT_MAX, 1200, VDD_RAW(1200), &pll2_tbl[3]},
-	{ 1, 1209600, PLL_2, 3, 0, UINT_MAX, 1200, VDD_RAW(1200), &pll2_tbl[4]},
-	{ 1, 1305600, PLL_2, 3, 0, UINT_MAX, 1250, VDD_RAW(1250), &pll2_tbl[5]},
-	{ 1, 1401600, PLL_2, 3, 0, UINT_MAX, 1250, VDD_RAW(1250), &pll2_tbl[6]},
-	{ 1, 1516800, PLL_2, 3, 0, UINT_MAX, 1250, VDD_RAW(1250), &pll2_tbl[7]},
+	{ 1, 1024000, PLL_2, 3, 0, UINT_MAX, 1200, VDD_RAW(1200), &pll2_tbl[1]},
+	{ 1, 1200000, PLL_2, 3, 0, UINT_MAX, 1200, VDD_RAW(1200), &pll2_tbl[2]},
+	{ 1, 1401600, PLL_2, 3, 0, UINT_MAX, 1250, VDD_RAW(1250), &pll2_tbl[3]},
+	{ 1, 1497600, PLL_2, 3, 0, UINT_MAX, 1250, VDD_RAW(1250), &pll2_tbl[4]},
 	{ 0 }
 };
 
